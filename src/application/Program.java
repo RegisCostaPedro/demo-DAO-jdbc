bc33b4f0eb7515d6202694080c41999f6bab95bd
Regis Costa pedro <regiscostapedro0705@gmail.com>
2024-01-10 11:11:51 -0300

    SellerDAO JDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.DAO.DAOFactory;
+import model.DAO.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"books");
         Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),2000.0,obj);
+
+        SellerDAO sellerDAO = DAOFactory.createSellerDAO();
 
         System.out.println(seller);
     }
